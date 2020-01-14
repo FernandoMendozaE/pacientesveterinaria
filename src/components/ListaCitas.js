68a9ec64d0a277f12a4deedbe6a60a760bdef97c
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-01-14 12:27:38 -0400

    Mostrando las Citas del State

---
+++ src/components/ListaCitas.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Cita from './Cita'
+
+const ListaCitas = ({ citas }) => {
+  return (
+    <div className="card mt-2 py-5">
+      <div className="card-body">
+        <h2 className="card-title text-center">Administra la citas aqui</h2>
+        <div className="lista-citas">
+          {citas.map(cita => (
+            <Cita key={cita.id} cita={cita} />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ListaCitas
